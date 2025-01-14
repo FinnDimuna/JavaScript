a767143cd79198cdf8c4c812bee4cb16737b5b94
Lofandr21 <lofandr@gmail.com>
2025-01-14 21:12:58 +0300

    Added descending order of sorting via second usage of NameHeader button

--- List sorting/script.js
+++ List sorting/script.js
@@ -3,6 +3,7 @@ for (let button of buttons) {
     button.style.fontSize = getComputedStyle(document.querySelector('th')).fontSize;
 }
 
+let sorted = 0;
 
 function NameSort() {
 
@@ -12,22 +13,23 @@ function NameSort() {
 
     let rows = tableBody.querySelectorAll(`tr`);
     let arrRows = Array.from(rows);
-    arrRows.sort((a, b) => {
-        let aText = a.querySelector('td:nth-of-type(1)').innerText;
-        let bText = b.querySelector('td:nth-of-type(2)').innerText;
-        return aText.localeCompare(bText);
-    });
 
+    if (sorted !== 1) {
+        sorted = 1;
+        arrRows.sort((a, b) => {
+            let aText = a.querySelector('td:nth-of-type(1)').innerText;
+            let bText = b.querySelector('td:nth-of-type(2)').innerText;
+            return aText.localeCompare(bText);
+        });
+    } else {
+        sorted = 0;
+        arrRows.sort((a, b) => {
+            let aText = a.querySelector('td:nth-of-type(1)').innerText;
+            let bText = b.querySelector('td:nth-of-type(2)').innerText;
+            return bText.localeCompare(aText);
+        });
+    }
     arrRows.forEach(row => tableBody.append(row));
 }
 
-
 document.getElementById('sort').addEventListener('click', NameSort);
-
-
-
-
-
-
-
-
